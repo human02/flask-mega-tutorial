8f44ee03120d1e8c903e21be3d9c32514883de26
human02 <42168454+human02@users.noreply.github.com>
2026-04-28 14:31:53 -0700

    feat: add config management and update dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 flask_app = Flask(__name__)
+flask_app.config.from_object(Config)
 
 from app import routes  # noqa: E402, F401 # avoid circular imports
