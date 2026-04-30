f9446e5bb1e98a6da1161e7615dda4faef230b44
human02 <42168454+human02@users.noreply.github.com>
2026-04-29 22:35:02 -0700

    feat: add Post model and migration for posts table creation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
+import sqlalchemy as sa  # includes general purpose database functions and classes such as types and query building helpers
+import sqlalchemy.orm as so  # provides the support for using models
 from app import db
+from datetime import datetime, timezone
 
 
 # defines the initial database structure (or schema)
@@ -11,5 +12,23 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    # for high-level view of the relationship between users and posts
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+
     def __repr__(self):  # it tells Python how to print objects of this class
         return f"<User {self.username!r}>"
+
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    # for high-level view of the relationship between users and posts
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return f"<Post {self.body!r}>"
