22eec07f21695f39496808f36bfa290e73d2792e
human02 <42168454+human02@users.noreply.github.com>
2026-05-01 11:04:11 -0700

    feat: integrate Flask-Login for user session management and enhance User model

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
 db = SQLAlchemy(flask_app)  # for DB connection
 migrate = Migrate(flask_app, db)  # for DB structure Migration needs
+login = LoginManager(flask_app)  # for user logged-in state
 
 from app import routes, models  # noqa: E402, F401 # avoid circular imports
--- app/models.py
+++ app/models.py
@@ -7,10 +7,12 @@ from werkzeug.security import (
     generate_password_hash,
     check_password_hash,
 )  # for password hash functionality
+from flask_login import UserMixin
+from app import login
 
 
 # defines the initial database structure (or schema)
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -42,3 +44,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.body!r}>"
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
