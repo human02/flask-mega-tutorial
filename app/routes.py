ccd02678d2ec9b14fe5c87b1d422337c9ec4f9a0
human02 <42168454+human02@users.noreply.github.com>
2026-05-01 11:11:17 -0700

    feat: implement logout functionality and update navigation links in base template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import flask_app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -33,3 +33,9 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
+
+
+@flask_app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
