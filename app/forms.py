d3727b1b647646fd74ab3a28682614c55ca34257
human02 <42168454+human02@users.noreply.github.com>
2026-05-03 14:57:50 -0700

    feat: add imports for SQLAlchemy and User model

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 
 class LoginForm(FlaskForm):
