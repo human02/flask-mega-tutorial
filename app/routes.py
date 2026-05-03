11cbbe1a34afe4de2de4ede17b708b7d32bdc23d
human02 <42168454+human02@users.noreply.github.com>
2026-05-03 15:34:25 -0700

    feat: implement user registration functionality and update login template

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ import sqlalchemy as sa
 from app import db
 from app.models import User
 from urllib.parse import urlsplit
+from app.forms import RegistrationForm
 
 
 @flask_app.route("/")
@@ -44,3 +45,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+
+@flask_app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
