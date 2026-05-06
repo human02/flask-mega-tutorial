47f04ba11c215cdbae973454552fea091b8e12aa
human02 <42168454+human02@users.noreply.github.com>
2026-05-05 19:34:07 -0700

    feat: add avatar method to User model for Gravatar integration

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ from werkzeug.security import (
 )  # for password hash functionality
 from flask_login import UserMixin
 from app import login
+from hashlib import md5  # for Avatar with Gravatar
 
 
 # defines the initial database structure (or schema)
@@ -29,6 +30,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
 
 class Post(db.Model):
